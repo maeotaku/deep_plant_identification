7de254d52d2044e8999e3a9d0bd1767793eca300
Jose Mario Carranza <jose.carranza.ro@gmail.com>
2018-02-15 20:53:31 -0600

    Cleaning up files, moving to more meaningful folders

--- FolderTools/separate_in_folders.py
+++ FolderTools/separate_in_folders.py
@@ -1,10 +1,25 @@
 from common import *
 
+#import xml
 import xml.etree.cElementTree as ET
+#from lxml import etree
+#import unidecode
+#import string
+#import codecs
+#from xml.parsers import expat
+
 
 def get_xml_field_value(file_path, field_name):
     try:
-        tree = ET.ElementTree(file=file_path)
+        #with codecs.open(file_path, 'r', 'utf-8') as f:
+        #    dat = f.read()
+        #dat = unidecode.unidecode(dat)
+        #dat = ''.join([str(char) for char in dat if char in string.printable])
+        #tree = xml.etree.ElementTree.fromstring(dat.encode('utf-8'))
+
+        parser = ET.XMLParser(encoding="UTF-8")
+        tree = ET.parse(file_path, parser=parser)
+
         for node in tree.findall(field_name):
             if not node is None:
                 return node.text
@@ -13,6 +28,7 @@ def get_xml_field_value(file_path, field_name):
     except Exception as e:
         print("Cannot read xml file", file_path, e)
         return None
+
 
 #most likely used with the Author tag, but can be anything else like a date
 def build_file_dictionary(start_path, field_name):
@@ -45,6 +61,7 @@ def get_first_words(name, k):
 def separate_in_folders(start_path, new_path, field_name="Species"):
     create_folder(new_path)
     field_species_files = build_file_dictionary(start_path, field_name)
+    print(field_species_files)
     for species_name in field_species_files.keys():
         print(species_name)
         output_folder = os.path.join(new_path, species_name)
@@ -54,8 +71,8 @@ def separate_in_folders(start_path, new_path, field_name="Species"):
 
 #ORIGINAL_PATH = "/Users/maeotaku/Documents/Datasets/PlantCLEFStandard/PlantCLEF2015/PlantCLEF2015TestDataWithAnnotations/"
 #NEW_PATH = "/Users/maeotaku/Documents/Datasets/PlantCLEFStandard/PlantCLEF2015/test_separated/"
-ORIGINAL_PATH = r"/Users/maeotaku/Documents/Datasets/PlantCLEFStandard/PlantCLEF2015/train/"
-NEW_PATH = r"/Users/maeotaku/Documents/Datasets/PlantCLEFStandard/PlantCLEF2015/train_separated/"
+ORIGINAL_PATH = r"/home/gmorales/Descarga_imagenes/eol_package/data/"
+NEW_PATH = r"/home/jcarranza/Datasets/RAW/PlantCLEF2018/"
 separate_in_folders(ORIGINAL_PATH, NEW_PATH, field_name="Species")
 
 #ORIGINAL_PATH = "/opt/data_plantclef/PlantCLEF/PlantCLEF2015Data/test/"
--- IDigBio/IDigBio_Pull_Data.py
+++
@@ -1,4 +0,0 @@
-import idigbio
-api = idigbio.json()
-record_list = api.search_records(rq={"scientificname": "Tabebuia impetiginosa", "hasImage": True})
-print(record_list)
